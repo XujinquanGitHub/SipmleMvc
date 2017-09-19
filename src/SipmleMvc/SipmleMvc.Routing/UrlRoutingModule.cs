33f66dcd4bc13b06d1f6025af05e7ef748f44657
许金泉 <xujinquan@tuhu.cn>
2017-09-19 19:25:18 +0800

    项目初步创建！

---
+++ src/SipmleMvc/SipmleMvc.Routing/UrlRoutingModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SipmleMvc.Routing
+{
+    public class UrlRoutingModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += Context_BeginRequest;
+        }
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext currentContext = sender as HttpContext;
+            if (currentContext != null)
+            {
+                currentContext.Response.Write("UrlRoutingModule");
+                currentContext.Response.End();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
