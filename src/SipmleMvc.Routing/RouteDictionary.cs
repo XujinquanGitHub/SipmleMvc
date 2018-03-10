c9a943c75d4ba8f9b429fd261fc410c8dfe75b78
许金泉 <xujinquan@tuhu.cn>
2018-03-10 12:09:15 +0800

    添加对物理文件路由判断！

--- src/SipmleMvc.Routing/RouteDictionary.cs
+++ src/SipmleMvc.Routing/RouteDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,25 @@ namespace SipmleMvc.Routing
 {
     public class RouteDictionary : Dictionary<string, Route>
     {
+        /// <summary>
+        /// 对现有文件应用路由
+        /// </summary>
+        public bool RouteExistingFiles { get; set; }
+
+
+
         public RouteData GetRouteData(HttpContext context)
         {
+            //不应该路由时，查看文件是否存在，如果存在返回Null
+            if (!RouteExistingFiles)
+            {
+                var flag = this.IsRouteToExistingFile(context);
+                if (flag)
+                {
+                    return null;
+                }
+            }
+
             foreach (var route in this.Values)
             {
                 RouteData routeData = route.GetRouteData(context);
@@ -21,5 +39,23 @@ namespace SipmleMvc.Routing
             }
             return null;
         }
+
+        /// <summary>
+        /// 访问的路径是否存在文件
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        private bool IsRouteToExistingFile(HttpContext httpContext)
+        {
+            string appRelativeCurrentExecutionFilePath = httpContext.Request.AppRelativeCurrentExecutionFilePath;
+            if (appRelativeCurrentExecutionFilePath == "~/" || !File.Exists(httpContext.Server.MapPath(appRelativeCurrentExecutionFilePath)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
     }
 }
